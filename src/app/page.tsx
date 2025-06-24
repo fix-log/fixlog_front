ea4a5c6dee4596f9e7aef9b3d15b9182bbc65a0c
00iun <lsh681236@naver.com>
2025-06-24 22:14:43 +0900

    init: 자잘한 기본세팅 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
     return (
         <>
-            <div className="bg-mainBlack">메인 페이지</div>
+            <div>메인 페이지</div>
         </>
     );
 }
-// 폴더구조 pages / features / entities / shared 4가지
-// 추가적인 라이브러리 설치 react-hook-form / Zod
