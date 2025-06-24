058410a8df4f4e613c616225a1a8b2f70293823b
00iun <lsh681236@naver.com>
2025-06-24 17:48:30 +0900

    init: 폴더구조 변경 / 컬러지정

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 툴: tailwindCSS, zustand, ESLint, prettier
 
 yarn dev로 실행가능
+
+사용 컬러 src/app/globsls.css 확인
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-    return <div>메인 페이지</div>
+    return (
+        <>
+            <div className="bg-mainBlack">메인 페이지</div>
+        </>
+    );
 }
+// 폴더구조 pages / features / entities / shared 4가지
+// 추가적인 라이브러리 설치 react-hook-form / Zod
