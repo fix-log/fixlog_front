b1fcf7d96a05b118cce44c722769c442b2378bc8
00iun <lsh681236@naver.com>
2025-06-27 04:04:23 +0900

    fix: 공통 컴포넌트 타입 수정

--- src/shared/form/ui/InputString.tsx
+++ src/shared/form/ui/InputString.tsx
@@ -2,30 +2,34 @@
 
 import FormRegister from '../model/FormRegister';
 import { FormErrorMessageType } from '../model/FormErrorMessage';
-import { FieldErrors, UseFormRegister } from 'react-hook-form';
-import { loginFormState } from '@/features/login/model/loginFormState';
+import { FieldErrors } from 'react-hook-form';
 
 interface InputStringProps {
     type: 'text' | 'number' | 'email' | 'password';
     name: keyof FormErrorMessageType;
-    errors: FieldErrors<loginFormState>;
     placeholder: string;
-    register: UseFormRegister<loginFormState>;
+    errors: FieldErrors;
+    register: any; //타입선언 해매다 일단 임시 any타입 선언
     label?: string;
     isRequired?: boolean;
 }
 
 export default function InputString({ type, name, errors, placeholder, register, label, isRequired }: InputStringProps) {
     return (
-        <div className='w-full'>
+        <div className="w-full">
             {label && (
                 <p className="cursor-default">
                     {label}
                     {isRequired && <span className="text-mainRed">*</span>}
                 </p>
             )}
-            <input className="border-1 border-gray4 rounded-[5px] text-[20px] w-full h-[60px] !pl-[17px] !pr-[20px] !my-[10px] focus:outline-none focus:border-pointDarkYellow/50 focus:shadow-[0_0_3.6px_#CDB200]" type={type} placeholder={placeholder} {...FormRegister({ register, name })} />
-            {errors[name] && <p className='text-pointDarkYellow !pl-3 !-mt-2'>{errors[name].message}</p>}
+            <input
+                className="border-1 border-gray4 rounded-[5px] text-[20px] w-full h-[60px] !pl-[17px] !pr-[20px] !my-[10px] focus:outline-none focus:border-pointDarkYellow/50 focus:shadow-[0_0_3.6px_#CDB200]"
+                type={type}
+                placeholder={placeholder}
+                {...FormRegister({ register, name })}
+            />
+            {errors[name] && <p className="text-pointDarkYellow !pl-3 !-mt-2">{errors[name].message}</p>}
         </div>
     );
 }
