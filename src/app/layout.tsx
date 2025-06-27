5266593f213f52f7180a44d8f6ad5b0208b75150
00iun <lsh681236@naver.com>
2025-06-27 12:29:57 +0900

    style: font 설정 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
+import localFont from 'next/font/local';
 import './globals.css';
+
+const font = localFont({
+    src: './SUIT-Regular.woff2',
+});
 
 export default function RootLayout({
     children,
@@ -6,7 +11,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="ko">
+        <html lang="ko" className={font.className}>
             <body>{children}</body>
         </html>
     );
