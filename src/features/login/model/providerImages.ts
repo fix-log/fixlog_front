a267414129e4c239be936dc1d3dd043feb9fb101
00iun <lsh681236@naver.com>
2025-06-27 12:31:57 +0900

    rename: 이미지 파일명 수정

--- src/features/login/model/providerImages.ts
+++ src/features/login/model/providerImages.ts
@@ -1,5 +1,5 @@
 export const ProviderImages = {
-    github: "/log_git.png",
-    naver: "/log_naver.png",
-    kakao: "/log_kakao.png"
+    github: "/logo_git.png",
+    naver: "/logo_naver.png",
+    kakao: "/logo_kakao.png"
 }
