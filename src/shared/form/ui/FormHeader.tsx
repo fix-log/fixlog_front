9b036f9f2fd84cc97856a24251b340400163c499
00iun <lsh681236@naver.com>
2025-06-27 13:01:11 +0900

    chore: 서버 클라이언트 전환

--- src/shared/form/ui/FormHeader.tsx
+++ src/shared/form/ui/FormHeader.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 interface FormHeaderProps {
     title: string;
 }
--- src/shared/form/ui/FormSubmitButton.tsx
+++ src/shared/form/ui/FormSubmitButton.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 interface FormSubmitButtonProps {
     text: string;
     isSubmitting: boolean;
