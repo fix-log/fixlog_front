ae4da0ebb352e9dc425687c1f8aa9d5932c5d396
Nagi-ei <nakt4453@gmail.com>
2025-06-29 04:57:49 +0900

    feature:링크 연결(nav)

--- src/app/workroom/[workroom-id]/issue/layout.tsx
+++ src/app/workroom/[workroom-id]/issue/layout.tsx
@@ -1,14 +1,28 @@
+import { NavigationItems } from '@/shared/types/navigation';
+import Link from 'next/link';
+
 interface LayoutProps {
+    params: { 'workroom-id': string };
     children: React.ReactNode;
 }
 
-export default async function Layout({ children }: LayoutProps) {
+const navItems: NavigationItems = [
+    { label: '모든 이슈', href: '/workroom/id/issue/all' },
+    { label: '내 이슈', href: '/workroom/id/issue/mine' },
+];
+
+export default async function Layout({ params, children }: LayoutProps) {
+    const { 'workroom-id': workroomId } = params;
+
     return (
         <>
             <nav>
                 <ul className="flex gap-4">
-                    <li>모든 이슈</li>
-                    <li>내 이슈</li>
+                    {navItems.map(item => (
+                        <li key={item.href}>
+                            <Link href={item.href.replace('id', workroomId)}>{item.label}</Link>
+                        </li>
+                    ))}
                 </ul>
             </nav>
             {children}
--- src/app/workroom/[workroom-id]/layout.tsx
+++ src/app/workroom/[workroom-id]/layout.tsx
@@ -1,18 +1,32 @@
+import { NavigationItems } from '@/shared/types/navigation';
+import Link from 'next/link';
+
 interface LayoutProps {
+    params: { 'workroom-id': string };
     children: React.ReactNode;
 }
 
-export default async function Layout({ children }: LayoutProps) {
+const navItems: NavigationItems = [
+    { label: '소개', href: '/workroom/id/info' },
+    { label: '멤버', href: '/workroom/id/member' },
+    { label: '이슈', href: '/workroom/id/issue' },
+    { label: '일정', href: '/workroom/id/schedule' },
+    { label: '관리', href: '/workroom/id/admin' },
+];
+
+export default async function Layout({ params, children }: LayoutProps) {
+    const { 'workroom-id': workroomId } = params;
+
     return (
         <>
             <div>프로젝트 제목</div>
             <nav>
                 <ul className="flex gap-4">
-                    <li>소개</li>
-                    <li>멤버</li>
-                    <li>이슈</li>
-                    <li>일정</li>
-                    <li>관리</li>
+                    {navItems.map(item => (
+                        <li key={item.href}>
+                            <Link href={item.href.replace('id', workroomId)}>{item.label}</Link>
+                        </li>
+                    ))}
                 </ul>
             </nav>
             {children}
