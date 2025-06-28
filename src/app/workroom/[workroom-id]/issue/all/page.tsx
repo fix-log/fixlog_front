e7743fe7cc55c92a82c0b9354355133afcf00dbc
Nagi-ei <nakt4453@gmail.com>
2025-06-29 05:45:56 +0900

    feat:레이아웃, 목업 (소개, 이슈, 일정)

--- src/app/workroom/[workroom-id]/issue/all/page.tsx
+++ src/app/workroom/[workroom-id]/issue/all/page.tsx
@@ -1,8 +1,77 @@
 export default async function Page() {
+    // TODO: 모든 이슈 데이터 가져오기
+
     return (
-        <>
-            <div>모든 이슈 페이지</div>
-            <div>해당 프로젝트의 포지션, 이슈 받아와서 렌더링하기</div>
-        </>
+        <section>
+            {/* 모든 이슈 페이지 */}
+            {/* 목업 */}
+            <div>
+                <h2 className="text-2xl font-bold">프론트엔드</h2>
+                <ul className="flex gap-2">
+                    <li>
+                        <h3>(아이콘) 여운</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                        <div className="border w-48 h-24">이슈 카드3</div>
+                    </li>
+                    <li>
+                        <h3>(아이콘) 나기</h3>
+                    </li>
+                    <li>
+                        <h3>(아이콘) 수정</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                        <div className="border w-48 h-24">이슈 카드3</div>
+                    </li>
+                    <li>
+                        <h3>(아이콘) 이운</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                    </li>
+                    <li>
+                        <h3>(아이콘) 미니</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h2 className="text-2xl font-bold">백엔드</h2>
+                <ul className="flex gap-2">
+                    <li>
+                        <h3>(아이콘) 네지</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                    </li>
+                    <li>
+                        <h3>(아이콘) 순해</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                    </li>
+                    <li>
+                        <h3>(아이콘) 병프</h3>
+                    </li>
+                    <li>
+                        <h3>(아이콘) 서농</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h2 className="text-2xl font-bold">디자인</h2>
+                <ul className="flex gap-2">
+                    <li>
+                        <h3>(아이콘) 혜진</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                    </li>
+                    <li>
+                        <h3>(아이콘) 다고운</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                    </li>
+                </ul>
+            </div>
+        </section>
     );
 }
--- src/app/workroom/[workroom-id]/issue/mine/page.tsx
+++ src/app/workroom/[workroom-id]/issue/mine/page.tsx
@@ -1,8 +1,36 @@
 export default async function Page() {
+    // TODO: 내 이슈 데이터 가져오기
+
     return (
-        <>
-            <div>내 이슈 페이지</div>
-            <div>해당 프로젝트의 내 이슈 받아와서 렌더링하기 (포지션도 표시)</div>
-        </>
+        <section>
+            {/* 내 이슈 페이지 */}
+            {/* 목업 */}
+            <div>
+                <h2 className="text-2xl font-bold">(아이콘) 여운 (총 갯수)</h2>
+                <ul className="flex gap-2">
+                    <li>
+                        <h3>시작 전</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                    </li>
+
+                    <li>
+                        <h3>진행 중</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                        <div className="border w-48 h-24">이슈 카드3</div>
+                    </li>
+
+                    <li>
+                        <h3>완료</h3>
+                        <div className="border w-48 h-24">이슈 카드1</div>
+                        <div className="border w-48 h-24">이슈 카드2</div>
+                        <div className="border w-48 h-24">이슈 카드3</div>
+                        <div className="border w-48 h-24">이슈 카드4</div>
+                        <div className="border w-48 h-24">이슈 카드5</div>
+                    </li>
+                </ul>
+            </div>
+        </section>
     );
 }
