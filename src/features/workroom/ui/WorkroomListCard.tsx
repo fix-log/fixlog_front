1422a4646abece8d615c6585609507ed27c3f78b
Nagi-ei <nakt4453@gmail.com>
2025-07-06 03:38:01 +0900

    feat: 워크룸 메인 +모달

--- src/features/workroom/ui/WorkroomListCard.tsx
+++ src/features/workroom/ui/WorkroomListCard.tsx
@@ -3,6 +3,7 @@ import ListCardButtons from './ListCardButtons';
 // TODO: 인자로 워크룸 받기
 export default async function WorkroomListCard() {
   return (
+    // TODO: 클릭 시 워크룸 상세 페이지로 이동 (받은 워크룸 id) (Link 태그로?)
     <div className='border-gray5 flex items-center justify-between rounded-[5px] border bg-white py-[32px] pr-[80px] pl-[25px]'>
       <div className='flex items-center gap-8'>
         {/* 질문 답변 받고나서 햄버거 아이콘 대체 (일단 필요성 자체에 의문) */}
@@ -15,7 +16,7 @@ export default async function WorkroomListCard() {
         </div>
       </div>
 
-      {/* TODO: 받은 워크룸에서 id 넘기기 */}
+      {/* TODO: 받은 워크룸에서 id 넘기기, 이벤트 버블링 방지하기 */}
       <ListCardButtons workroomId='123' />
     </div>
   );
--- src/widgets/ui/WorkroomPlusButton.tsx
+++ src/widgets/ui/WorkroomPlusButton.tsx
@@ -1,18 +1,53 @@
 'use client';
 
+import Modal from '@/shared/ui/Modal';
+import { FilePlus, List } from 'lucide-react';
+import Link from 'next/link';
+import { useState } from 'react';
+
 export default function WorkroomPlusButton() {
-  const handleClick = () => {
-    // TODO: 모달? 드롭다운? 오픈
-    console.log('clicked');
-  };
+  const [isModalOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <button
-      onClick={handleClick}
-      className='bg-mainRed fixed right-35 bottom-35 flex h-20 w-20 items-center justify-center rounded-full hover:cursor-pointer'
-    >
-      <span className='text-5xl text-white'>+</span>
-    </button>
+    <>
+      <button
+        onClick={() => setIsOpen(!isModalOpen)}
+        className='bg-mainRed fixed right-35 bottom-35 z-100 flex h-20 w-20 items-center justify-center rounded-full hover:cursor-pointer'
+      >
+        <span
+          className={`${isModalOpen && 'rotate-45'} text-5xl text-white transition-all duration-200`}
+        >
+          +
+        </span>
+      </button>
+
+      {isModalOpen && (
+        <>
+          <Modal setIsOpen={setIsOpen} className='inset-auto right-37 bottom-63 px-8 py-3'>
+            <Link href='/workroom/mine' className='flex items-center justify-end gap-4 py-7'>
+              <div className='flex flex-col items-end justify-between pl-3'>
+                <span className='text-[22px] font-extrabold'>워크룸 리스트</span>
+                <span className='text-gray3 text-[14px] font-semibold'>
+                  내가 만든 워크룸 리스트 보기
+                </span>
+              </div>
+              <List className='text-mainRed size-9' />
+            </Link>
+
+            <div className='border-gray5 bg-gray5 h-[1px] w-full px-5' />
+
+            <Link href='/workroom/create' className='flex items-center justify-end gap-4 py-7'>
+              <div className='flex flex-col items-end justify-between'>
+                <span className='text-[22px] font-extrabold'>나의 워크룸 생성</span>
+                <span className='text-gray3 text-[14px] font-semibold'>워크룸 만들기</span>
+              </div>
+              <FilePlus className='text-mainRed size-9' />
+            </Link>
+          </Modal>
+          <div className='fixed right-40.5 bottom-59 z-100 border-t-[16px] border-r-[16px] border-l-[16px] border-t-white border-r-transparent border-l-transparent' />
+        </>
+      )}
+    </>
   );
 }
 
