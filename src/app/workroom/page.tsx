b9a184e10db0ce290603c008e074ae0e401c6c88
Nagi-ei <nakt4453@gmail.com>
2025-07-07 22:01:48 +0900

    design: 워크룸 만들기 컴포넌트 모바일 디자인

--- src/app/workroom/page.tsx
+++ src/app/workroom/page.tsx
@@ -13,14 +13,14 @@ const myWorkrooms = [
     status: '진행중',
     member: 10,
   },
-  {
-    id: '2',
-    name: '워크룸 2',
-    description: '25자 정도 (띄어쓰기 포함) 소개글을 쓸 수 있어요.',
-    period: '2025.01.01 ~ 2025.01.01',
-    status: '진행중',
-    member: 10,
-  },
+  // {
+  //   id: '2',
+  //   name: '워크룸 2',
+  //   description: '25자 정도 (띄어쓰기 포함) 소개글을 쓸 수 있어요.',
+  //   period: '2025.01.01 ~ 2025.01.01',
+  //   status: '진행중',
+  //   member: 10,
+  // },
 ];
 
 // 임시 참여한 워크룸 데이터
@@ -83,21 +83,23 @@ export default function Workroom() {
 
       <section className='flex w-full flex-col gap-5 pt-6 md:gap-10 md:pt-24'>
         <Link href='/workroom/mine' className='w-fit'>
-          <h2 className='text-[32px] font-extrabold'>👑 여운님이 만든 워크룸 {'>'}</h2>
+          <h2 className='text-[18px] font-extrabold md:text-[32px]'>
+            👑 여운님이 만든 워크룸 {'>'}
+          </h2>
         </Link>
 
         {/* 워크룸 없을 때 */}
-        <div className='bg-mainRed/4 flex gap-4 py-16'>
+        <div className='md:bg-mainRed/4 flex gap-4 md:py-16'>
           <Link
             href={`/workroom/create`}
-            className='mx-auto transition-all duration-200 hover:scale-102'
+            className='mx-auto transition-all duration-200 hover:scale-102 max-md:w-full'
           >
-            <div className='border-mainRed/40 flex flex-col items-center justify-center gap-4 rounded-[5px] border bg-white px-18.5 py-20 shadow-[0_0_13.5px_0_rgba(255,68,39,0.2)]'>
-              <div className='bg-mainRed mb-5.5 flex h-15 w-15 items-center justify-center rounded-full'>
-                <span className='text-5xl text-white'>+</span>
+            <div className='border-gray5 md:border-mainRed/40 flex flex-col items-center justify-center gap-2 rounded-[5px] border bg-white px-5 py-20 md:gap-4 md:px-18.5 md:shadow-[0_0_13.5px_0_rgba(255,68,39,0.2)]'>
+              <div className='bg-mainRed mb-2 flex h-10 w-10 items-center justify-center rounded-full md:mb-5.5 md:h-15 md:w-15'>
+                <span className='text-3xl text-white md:text-5xl'>+</span>
               </div>
-              <h3 className='text-[32px] font-extrabold'>워크룸 만들기</h3>
-              <p className='text-gray3 text-[18px]'>
+              <h3 className='text-[16px] font-extrabold md:text-[32px]'>워크룸 만들기</h3>
+              <p className='text-gray3 text-[12px] md:text-[18px]'>
                 팀원들과 함께 프로젝트를 할 수 있는 워크룸을 만들어보세요!
               </p>
             </div>
