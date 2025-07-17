1000fa820e4a422feea38e414283ea3b5fbee6a2
00iun <lsh681236@naver.com>
2025-07-17 22:56:59 +0900

    ✨Feat: 로그인상태, 정보  전역관리 추가

--- src/features/login/api/loginAPI.ts
+++ src/features/login/api/loginAPI.ts
@@ -1,19 +1,15 @@
-// 해당 파일은 임시 파일로
-// API 준비되면 갈아엎어질 파일
-
-import { FieldErrors } from 'react-hook-form';
-import { LoginFormStateType } from '../model/loginFormState';
-
-interface LoginAPIProps {
-  email: string;
-  password: string;
-  errors: FieldErrors<LoginFormStateType>;
-}
-
-export default function loginAPI({ email, password, errors }: LoginAPIProps) {
-  const data = {
-    email: 'true@gmail.com',
-    password: '123456@A',
+interface fetchLoginProps {
+  loginData: {
+    email: string;
+    password: string;
   };
-  return Object.keys(errors).length === 0 && data.email === email && data.password === password;
 }
+
+export const loginAPI = async ({ loginData }: fetchLoginProps) => {
+  const respones = await fetch('/api/user/login', {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(loginData),
+  });
+  return respones.json();
+};
