0cfe8e5d658d2878659643599b70864c1ea636b8
yeowun05-12 <taking0n@naver.com>
2025-07-23 01:59:53 +0900

    💄 CustomDropdown 추가

--- src/app/(main)/notification/page.tsx
+++ src/app/(main)/notification/page.tsx
@@ -7,7 +7,7 @@ const hasNoti = false;
 
 export default function Notification() {
   return (
-    <div className='flex min-h-[70vh] max-w-3xl flex-col pt-12'>
+    <div className='flex flex-col pt-12'>
       {/* 항상 페이지 상단에 고정 */}
       <NotiHeader />
       <div className='flex flex-1 flex-col'>
--- src/features/notification/ui/NotiEmpty.tsx
+++ src/features/notification/ui/NotiEmpty.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function NotificationEmpty() {
   return (
     <div className='flex flex-1 flex-col items-center justify-center gap-2 text-gray-400'>
-      <Image src='/x_emput.png' alt='x_emput' width={80} height={80} className='mb-[25px]' />
+      <Image src='/x_emput.png' alt='x_emput' width={60} height={60} className='mb-[25px]' />
       <div className='text-gray4 text-[24px] font-bold'>아직 알림이 오지 않았습니다</div>
     </div>
   );
