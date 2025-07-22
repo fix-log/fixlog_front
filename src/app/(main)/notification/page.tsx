231b00ac6ae1c10c9ee8d3e495f98858ff65c644
yeowun05-12 <taking0n@naver.com>
2025-07-23 00:53:48 +0900

    💄 알림 진행중

--- src/app/(main)/notification/page.tsx
+++ src/app/(main)/notification/page.tsx
@@ -1,15 +1,22 @@
 'use client';
 
-import ToggleSwitch from '@/shared/ui/ToggleSwitch';
+import NotiHeader from '@/widgets/notification/NotiHeader';
+import NotiEmpty from '@/features/notification/ui/NotiEmpty';
+
+const hasNoti = false;
 
 export default function Notification() {
   return (
-    <div className='flex'>
-      <div>
-        <div className='flex'>
-          <h2 className='text-h3 font-extrabold'>알림</h2>
-          <ToggleSwitch />
-        </div>
+    <div className='flex min-h-[70vh] max-w-3xl flex-col pt-12'>
+      {/* 항상 페이지 상단에 고정 */}
+      <NotiHeader />
+      <div className='flex flex-1 flex-col'>
+        {hasNoti ? (
+          // 알림 리스트 컴포넌트 (예시)
+          <div> {/* NotificationList 들어올 자리 */} </div>
+        ) : (
+          <NotiEmpty />
+        )}
       </div>
     </div>
   );
