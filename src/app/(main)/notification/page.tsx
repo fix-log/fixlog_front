dee275e0c3ef9d5296baef3599b09af864ea0562
yeowun05-12 <taking0n@naver.com>
2025-07-23 03:37:35 +0900

    💄 알림 UI 작업 완료

--- src/app/(main)/notification/page.tsx
+++ src/app/(main)/notification/page.tsx
@@ -2,22 +2,16 @@
 
 import NotiHeader from '@/widgets/notification/NotiHeader';
 import NotiEmpty from '@/features/notification/ui/NotiEmpty';
+import NotiList from '@/widgets/notification/NotiList';
 
-const hasNoti = false;
+const hasNoti = true;
 
 export default function Notification() {
   return (
-    <div className='flex flex-col pt-12'>
+    <div className='flex h-[100vh] flex-col pt-12'>
       {/* 항상 페이지 상단에 고정 */}
       <NotiHeader />
-      <div className='flex flex-1 flex-col'>
-        {hasNoti ? (
-          // 알림 리스트 컴포넌트 (예시)
-          <div> {/* NotificationList 들어올 자리 */} </div>
-        ) : (
-          <NotiEmpty />
-        )}
-      </div>
+      <div className='flex flex-1 flex-col'>{hasNoti ? <NotiList /> : <NotiEmpty />}</div>
     </div>
   );
 }
--- src/widgets/notification/NotiHeader.tsx
+++ src/widgets/notification/NotiHeader.tsx
@@ -6,7 +6,7 @@ import ToggleSwitch from '@/shared/ui/ToggleSwitch';
 
 export default function NotiHeader() {
   return (
-    <div className=''>
+    <div className='mt-[50px] mb-[50px]'>
       <div className='flex w-[100vw] max-w-[1440px] items-center justify-between gap-6 px-7'>
         <div className='flex items-center'>
           <h2 className='text-h3 pr-[10px] font-extrabold'>알림</h2>
