82efd003e6e6e9dae7a594c78b7f457850fa392d
00iun <lsh681236@naver.com>
2025-08-05 13:00:55 +0900

    ✨ 이탈 안내 모달 추가

--- src/features/profile/LeaveConfirmModal.tsx
+++ src/features/profile/LeaveConfirmModal.tsx
@@ -1,27 +1,51 @@
-import Modal from '@/shared/ui/Modal';
+'use client';
 
-export default function LeaveConfirmModal({ setOpen }) {
-  return <Modal setIsOpen={setOpen}>
-    <div className="p-4">
-      <h2 className="text-lg font-semibold">변경 사항이 저장되지 않습니다.</h2>
-      <p className="mt-2">정말로 페이지를 떠나시겠습니까?</p>
-      <div className="mt-4 flex justify-end space-x-2">
+import { colorChangeAnimation } from '@/shared/ui/Animation';
+import Modal from '@/shared/ui/Modal';
+import { useRouter } from 'next/navigation';
+import { Dispatch, SetStateAction } from 'react';
+
+interface LeaveConfirmModalProps {
+  setOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function LeaveConfirmModal({ setOpen }: LeaveConfirmModalProps) {
+  const router = useRouter();
+
+  return (
+    <Modal className='flex px-[75px] py-[70px]' setIsOpen={setOpen}>
+      <h1 className='text-[35px] font-extrabold'>저장하지 않은</h1>
+      <h1 className='text-[34px] font-extrabold'>변경사항이 있습니다</h1>
+      <div className='text-gray3 text-h5 flex flex-col items-center pt-[14px] pb-[42px]'>
+        <p className='pb-1 font-semibold'>지금 나가시면 변경하신 내용은 반영되지 않습니다</p>
+      </div>
+      <div className='flex w-full justify-evenly font-extrabold'>
         <button
-          className="px-4 py-2 bg-gray-200 rounded hover:bg-gray-300"
-          onClick={() => setOpen(false)}
-        >
-          취소
-        </button>
-        <button
-          className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+          className={
+            'bg-mainBlack max-h-[45px] w-full max-w-[135px] cursor-pointer rounded-[5px] py-[10px] text-white hover:bg-black ' +
+            colorChangeAnimation
+          }
+          type='button'
           onClick={() => {
+            router.back();
             setOpen(false);
-            window.history.back();
           }}
         >
-          떠나기
+          나가기
+        </button>
+        <button
+          className={
+            'bg-mainRed hover:bg-mainDarkRed max-h-[45px] w-full max-w-[135px] cursor-pointer rounded-[5px] py-[10px] text-white ' +
+            colorChangeAnimation
+          }
+          type='button'
+          onClick={() => {
+            setOpen(false);
+          }}
+        >
+          머무르기
         </button>
       </div>
-    </div>
-  </Modal>;
+    </Modal>
+  );
 }
