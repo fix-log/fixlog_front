47a5df3458358e7b59aed207991c539566dfcb5e
00iun <lsh681236@naver.com>
2025-08-09 07:22:49 +0900

    ✨ 카테고리 상태 -> 라우팅으로 변경

--- src/widgets/userInfo/CategoryTabs.tsx
+++ src/widgets/userInfo/CategoryTabs.tsx
@@ -1,6 +1,12 @@
 import { userInfoStore } from '@/entities/userInfo/UserInfoStore';
 import { colorChangeAnimation } from '@/shared/ui/Animation';
+import { useRouter } from 'next/navigation';
 import { ReactNode, useState } from 'react';
+
+type dd = {
+  label: string;
+  value: string;
+};
 
 interface CategoryTabsProps {
   children: ReactNode;
@@ -8,9 +14,14 @@ interface CategoryTabsProps {
 
 export default function CategoryTabs({ children }: CategoryTabsProps) {
   const isMe = userInfoStore((s) => s.userInfo.isMe);
-  const category = ['유저 정보', '픽레드', isMe ? '크루 모집' : '워크룸'] as const;
+  const category = [
+    { label: '유저 정보', value: 'info' },
+    { label: '픽레드', value: 'fixred' },
+    isMe ? { label: '크루 모집', value: 'crew' } : { label: '워크룸', value: 'workroom' },
+  ];
   const [isSelectedCategory, setIsSelectedCategory] = useState('유저 정보');
-  function selectedStyle(item: (typeof category)[number]) {
+  const router = useRouter();
+  function selectedStyle(item: string) {
     if (isSelectedCategory === item) return ' border-b-mainBlack text-black font-bold ';
     return '';
   }
@@ -20,15 +31,18 @@ export default function CategoryTabs({ children }: CategoryTabsProps) {
       <div className='border-gray4 text-gray4 mt-6 flex h-[90px] border-t text-[20px]'>
         {category.map((item) => (
           <button
-            key={item}
-            onClick={() => setIsSelectedCategory(item)}
+            key={item.label}
+            onClick={() => {
+              setIsSelectedCategory(item.label)
+              router.push(`${item.value}`);
+            }}
             className={
               'border-gray4 h-full grow cursor-pointer border-b-2' +
-              selectedStyle(item) +
+              selectedStyle(item.label) +
               colorChangeAnimation
             }
           >
-            {item}
+            {item.label}
           </button>
         ))}
       </div>
